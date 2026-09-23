542a88842da089f51072e4352bace16f18c8f46e
Light-2-Play <92185180+Light-2-Play@users.noreply.github.com>
2026-09-23 19:55:17 +0300

    Update build.gradle.kts

--- apps/multiplatform/android/build.gradle.kts
+++ apps/multiplatform/android/build.gradle.kts
@@ -192,7 +192,7 @@ dependencies {
     //androidTestImplementation("androidx.compose.ui:ui-test-junit4:$compose_version")
     debugImplementation("androidx.compose.ui:ui-tooling:1.6.4")
 
-   val cameraxVersion = "1.3.4"
+   val cameraxVersion = "1.4.0"
     implementation("androidx.camera:camera-core:$cameraxVersion")
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
